e1ecd604a60803f01996b9cc8dee2fc37395fa87
Vadim Hatsura <vadim.hatsura@gmail.com>
2022-08-10 00:34:40 +0200

    add web example project

---
+++ examples/WebExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebExample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IProducer<Null, WeatherForecast> _producer;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,
+        IProducer<Null, WeatherForecast> producer)
+    {
+        _logger = logger;
+        _producer = producer;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [HttpPost(Name = "PostWeatherForecast")]
+    public async Task<IActionResult> Post(WeatherForecast forecast, CancellationToken cancellationToken)
+    {
+        await _producer.ProduceAsync("forecasts", new Message<Null, WeatherForecast> { Value = forecast },
+            cancellationToken);
+
+        return Ok();
+    }
+}
---
+++ examples/WebExample/Program.cs
@@ -0,0 +1,75 @@
+using Confluent.Kafka;
+using Confluent.Kafka.Extensions.Diagnostics;
+using Confluent.Kafka.Extensions.OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using WebExample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+var kafkaServers = "localhost:9092";
+
+builder.Services.AddSingleton(_ =>
+{
+    return new ProducerBuilder<Null, WeatherForecast>(
+            new ProducerConfig(new ClientConfig { BootstrapServers = kafkaServers }))
+        .SetKeySerializer(Serializers.Null)
+        .SetValueSerializer(new KafkaJsonSerializer<WeatherForecast>())
+        .BuildWithInstrumentation();
+});
+
+builder.Services.AddSingleton(_ =>
+{
+    return new ConsumerBuilder<Ignore, WeatherForecast>(
+            new ConsumerConfig(new ClientConfig { BootstrapServers = kafkaServers })
+            {
+                GroupId = "group1", AutoOffsetReset = AutoOffsetReset.Earliest
+            })
+        .SetValueDeserializer(new KafkaJsonSerializer<WeatherForecast>())
+        .Build();
+});
+
+builder.Services.AddHostedService<WeatherForecastConsumerService>();
+
+builder.Services.AddOpenTelemetryTracing(traceBuilder =>
+{
+    traceBuilder.AddSource("webExample")
+        .SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("webExample", serviceVersion: "1.0.0")
+            .AddAttributes(new[]
+            {
+                new KeyValuePair<string, object>("deployment.environment",
+                    builder.Environment.EnvironmentName)
+            }))
+        .AddZipkinExporter(o =>
+        {
+            o.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
+        })
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddConfluentKafkaInstrumentation();
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
