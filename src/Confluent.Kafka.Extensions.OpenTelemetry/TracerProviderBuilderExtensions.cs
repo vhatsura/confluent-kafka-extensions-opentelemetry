f44088237be46fcb5ec314a768d879ada8872a52
Vadim Hatsura <vadim.hatsura@gmail.com>
2022-08-10 00:33:05 +0200

    add kafka instrumentation initialization (#1)

---
+++ src/Confluent.Kafka.Extensions.OpenTelemetry/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using OpenTelemetry.Trace;
+
+namespace Confluent.Kafka.Extensions.OpenTelemetry;
+
+public static class TracerProviderBuilderExtensions
+{
+    public static TracerProviderBuilder AddConfluentKafkaInstrumentation(this TracerProviderBuilder builder)
+    {
+        if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+        builder.AddSource("Confluent.Kafka.Extensions.Diagnostics");
+
+        return builder;
+    }
+}
