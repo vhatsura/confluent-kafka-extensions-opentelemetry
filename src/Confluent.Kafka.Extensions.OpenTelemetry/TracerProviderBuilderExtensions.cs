c93e763bf3f48402ec9769a8385651729b1a80c8
Vadim Hatsura <vadim.hatsura@gmail.com>
2022-08-10 13:26:24 +0200

    add nuget metadata (#5)

--- src/Confluent.Kafka.Extensions.OpenTelemetry/TracerProviderBuilderExtensions.cs
+++ src/Confluent.Kafka.Extensions.OpenTelemetry/TracerProviderBuilderExtensions.cs
@@ -1,9 +1,15 @@
-﻿using OpenTelemetry.Trace;
+using OpenTelemetry.Trace;
 
 namespace Confluent.Kafka.Extensions.OpenTelemetry;
 
+/// <summary>
+///     Extension methods for <see cref="TracerProviderBuilder" />.
+/// </summary>
 public static class TracerProviderBuilderExtensions
 {
+    /// <summary>
+    ///     Enables automatic data collection for the Confluent.Kafka client.
+    /// </summary>
     public static TracerProviderBuilder AddConfluentKafkaInstrumentation(this TracerProviderBuilder builder)
     {
         if (builder == null) throw new ArgumentNullException(nameof(builder));
